4c5dfca0148fdbdf0803ddf7a2c205352edee6cd
Mauricio Freitas <contato@mauriciofreitas.eng.br>
2022-11-04 20:31:43 -0300

    Plot cart position

--- pendulum/cart/plot.py
+++ pendulum/cart/plot.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import click
 from plotly import graph_objects as go
+from plotly.subplots import make_subplots
 
 from pendulum import settings
 from pendulum.plot import load_recording
@@ -12,8 +13,19 @@ def plot_recording(path: Path, height: int, width: int) -> None:
     """Plot Recording data."""
     df = load_recording(path)
 
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=df.index, y=df["angle"], name="Angle (deg)"))
+    fig = make_subplots(rows=2, cols=1)
+
+    fig.add_trace(
+        go.Scatter(x=df.index, y=df["angle"], name="Angle (deg)"),
+        row=1,
+        col=1,
+    )
+    fig.add_trace(
+        go.Scatter(x=df.index, y=df["cart_x"], name="Cart Position"),
+        row=2,
+        col=1,
+    )
+
     plot_path = settings.PLOT_PATH / f"{path.stem}.png"
     fig.update_layout(
         title="Cart Pendulum Simulation.",
@@ -21,5 +33,6 @@ def plot_recording(path: Path, height: int, width: int) -> None:
         width=width,
         height=height,
     )
+
     fig.write_image(plot_path)
     click.secho(f'Plot generated: "{plot_path.name}"', fg="green")
